48a47c1ccdc4bec7f3cbc0d95fbee68344308168
s174270 <s174270@student.dtu.dk>
2019-11-21 23:17:24 +0900

    flutter format

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'flip_clock.dart';
 
-
 void main() {
   runApp(ClockCustomizer((ClockModel model) => FlipClock(model)));
 }
